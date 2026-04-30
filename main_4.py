b106032d7a0edf231523938eabff2c850359c36c
iotmaker-cc <60481039+iotmaker-cc@users.noreply.github.com>
2026-04-30 18:38:18 +0900

    update: main_4.py

--- main_4.py
+++ main_4.py
@@ -1,7 +1,7 @@
-# PocketIDE에 오신 것을 환영합니다!
-# 아래 코드를 수정하고 ▶ 실행 버튼을 눌러보세요.
+name = "PocketIDE"
+age = 3
+version = 1.0
+is_ready = True
 
-def greet(name):
-    print(f"안녕하세요, {name}!")
-
-greet("PocketIDE")
+print(name)
+print(type(age))

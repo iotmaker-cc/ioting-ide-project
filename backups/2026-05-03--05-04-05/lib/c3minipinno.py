0f34d6938129451320c0a66ac86764cd1adac2c4
iotmaker-cc <60481039+iotmaker-cc@users.noreply.github.com>
2026-05-03 14:04:23 +0900

    MicroPython Backup: /lib/c3minipinno.py

---
+++ backups/2026-05-03--05-04-05/lib/c3minipinno.py
@@ -0,0 +1,21 @@
+# c3minipinno.py --> 개발보드 \lib\pinno.py
+# c3picopinno.py --> 개발보드 \lib\pinno.py
+
+# 내장 핀 
+# BUILTIN_RGB = 7
+BUILTIN_BTN = 9
+
+# 외부핀 사용
+R1 = TX = 21
+R2 = RX = 20
+R3 = D1 = SCL = RELAY = 10
+R4 = D2 = SDA = DS18B20 = WS2812B = 8
+R5 = D3 = BUILTIN_RGB = PIR = BTN = 7
+R6 = D4 = DHT = DHT11 = DHT22 = 6
+
+L2 = A0 = POTENTIOMERER = 3
+L3 = D0 = BUTTON_1 = 2
+L4 = D5 = SCK  = BUTTON_2 = 1
+L5 = D6 = MISO = LED_1 = 0
+L6 = D7 = MOSI = LED_2 = 4
+L7 = D8 = CS   = BUZZER = 5

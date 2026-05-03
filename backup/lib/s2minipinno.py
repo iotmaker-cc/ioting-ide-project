86bb74ec425fcb205f50a26bf4365600e67c1598
iotmaker-cc <60481039+iotmaker-cc@users.noreply.github.com>
2026-05-03 15:53:39 +0900

    MicroPython Backup: /lib/s2minipinno.py

---
+++ backup/lib/s2minipinno.py
@@ -0,0 +1,36 @@
+# s2minipinno.py --> 개발보드 \lib\pinno.py
+
+# 내장 핀 
+BUILTIN_LED = 15
+BUILTIN_BTN = 0
+
+# 외부핀 사용
+R1 = TX = 39
+R2 = RX = 37
+R3 = D1 = SCL = RELAY = 35
+R4 = D2 = SDA = DS18B20 = WS2812B = 33
+R5 = D3 = PIR = BTN = 18
+R6 = D4 = DHT = DHT11 = DHT22 = 16
+
+L2 = A0 = POTENTIOMETER =3
+L3 = D0 = BUTTON_1 = 5
+L4 = D5 = SCK  = BUTTON_2 = 7
+L5 = D6 = MISO = LED_1 = 9
+L6 = D7 = MOSI = LED_2 = 11
+L7 = D8 = CS   = BUZZER = 12
+
+# 내부핀 사용
+R1_IN = TX_IN = 40
+R2_IN = RX_IN = 38
+R3_IN = D1_IN = SDA_IN = RELAY_IN = 36
+R4_IN = D2_IN = SCL_IN = DS18B20_IN = WS2812B_IN = 34
+R5_IN = D3_IN = PIR_IN = BTN_IN = 21
+R6_IN = D4_IN = DHT_IN = DHT11_IN = DHT22_IN = 17
+
+L1_IN         = 1
+L2_IN = A0_IN = POTENTIOMETER_IN = 2
+L3_IN = D0_IN = BUTTON_1_IN = 4
+L4_IN = D5_IN = BUTTON_2_IN = 6
+L5_IN = D6_IN = LED_1_IN = 8
+L6_IN = D7_IN = LED_2_IN = 10
+L7_IN = D8_IN = BUZZER_IN = 13

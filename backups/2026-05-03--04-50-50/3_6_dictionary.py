ed88ec5d9c63cbde3358899536343fc99e069968
iotmaker-cc <60481039+iotmaker-cc@users.noreply.github.com>
2026-05-03 13:51:20 +0900

    MicroPython Backup: /3_6_dictionary.py

---
+++ backups/2026-05-03--04-50-50/3_6_dictionary.py
@@ -0,0 +1,14 @@
+# 딕셔너리 생성
+my_dict = {
+    "name": "John",
+    "age": 30,
+    "city": "New York",
+    "is_student": False
+}
+
+# 딕셔너리 출력
+print(my_dict)
+
+# 딕셔너리의 특정 값에 접근하여 출력
+print("Name:", my_dict["name"])
+print("Age:", my_dict["age"])

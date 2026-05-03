4806fd0e0f268825c016234795420d09c8b8b739
iotmaker-cc <60481039+iotmaker-cc@users.noreply.github.com>
2026-05-03 13:51:18 +0900

    MicroPython Backup: /3_5_tuple.py

---
+++ backups/2026-05-03--04-50-50/3_5_tuple.py
@@ -0,0 +1,13 @@
+# RGB 색상을 표현하는 튜플 예제
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+# RGB 색상 튜플 출력
+print("Red:", red)
+print("Green:", green)
+print("Blue:", blue)
+print("White:", white)
+print("Black:", black)

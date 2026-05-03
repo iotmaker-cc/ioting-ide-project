3a412aafb88c1de0e3c6cb536978399ada0f1034
iotmaker-cc <60481039+iotmaker-cc@users.noreply.github.com>
2026-05-03 13:51:50 +0900

    MicroPython Backup: /4_1_arithmetic_operators.py

---
+++ backups/2026-05-03--04-50-50/4_1_arithmetic_operators.py
@@ -0,0 +1,29 @@
+# 덧셈
+a = 10
+b = 5
+sum_result = a + b
+print("덧셈:", sum_result)  # 출력: 덧셈: 15
+
+# 뺄셈
+sub_result = a - b
+print("뺄셈:", sub_result)  # 출력: 뺄셈: 5
+
+# 곱셈
+mul_result = a * b
+print("곱셈:", mul_result)  # 출력: 곱셈: 50
+
+# 나눗셈
+div_result = a / b
+print("나눗셈:", div_result)  # 출력: 나눗셈: 2.0
+
+# 나머지 (모듈로 연산)
+mod_result = a % b
+print("나머지:", mod_result)  # 출력: 나머지: 0
+
+# 거듭제곱
+exp_result = a ** 2
+print("거듭제곱:", exp_result)  # 출력: 거듭제곱: 100
+
+# 정수 나눗셈 (몫만 반환)
+int_div_result = a // b
+print("정수 나눗셈:", int_div_result)  # 출력: 정수 나눗셈: 2
